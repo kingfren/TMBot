8c4aa1eababc1f659435803098482fe27b3269e6
noreph <norep@msn.com>
2023-03-19 22:46:14 +0800

    Sun 19 Mar 22:46:14 CST 2023

--- __main__.py
+++ __main__.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
     load_plugin()
     client.start()
     me = client.get_me()
-    print(me.username)
     name = me.username if me.username else me.first_name + me.last_name if me.first_name and me.last_name else me.first_name
     logger.info(f"{name}，嗨！请在任意对话框发送 {prefix}pm help pm 获取帮助~")
     idle()
